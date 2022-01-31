188702524c97a31113a070e8cf2531bb690f37ad
Miguel Angel Castillo Cornejo <mikedosce1992@gmail.com>
2022-01-31 01:01:45 -0600

    Not found page added

--- Web/Controllers/ErrorController.cs
+++ Web/Controllers/ErrorController.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Web.Controllers
 {
     public class ErrorController : Controller
     {
-        public IActionResult Index()
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            switch (statusCode)
+            {
+                case 404:
+                    return View("404");
+            }
+
             return View();
         }
     }
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -46,10 +46,12 @@ namespace Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePagesWithRedirects("/error/{0}");
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithRedirects("/error/{0}");
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
