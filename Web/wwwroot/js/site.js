3c9d32876b7e5b5c81951c855186e24bb095c1e8
Miguel Angel Castillo Cornejo <mikedosce1992@gmail.com>
2022-02-01 18:01:43 -0600

    Configure TinyMCE plugin in order to allows more functions

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -3,11 +3,6 @@
 
 // Write your JavaScript code.
 
-tinymce.init({
-    selector: '#Body',
-    height: 400
-});
-
 function onDeleteNote(e) {
     if (confirm("Are you sure you want to delete this note?"))
         return true;
@@ -49,4 +44,49 @@ $("button.searchbar__button").click(e => {
         input.classList.add("show");
         input.focus();
     }
+});
+
+/**
+ * Configure the TinyMCE html text editor
+ */
+
+tinymce.init({
+    selector: '#Body',
+    height: 400,
+    plugins: 'print preview importcss tinydrive searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+    mobile: {
+        plugins: 'print preview importcss tinydrive searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount textpattern noneditable help charmap quickbars emoticons'
+    },
+    menu: {
+        tc: {
+            title: 'Comments',
+            items: 'addcomment showcomments deleteallconversations'
+        }
+    },
+    menubar: 'file edit view insert format tools table tc help',
+    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+    autosave_ask_before_unload: true,
+    autosave_interval: '30s',
+    autosave_prefix: '{path}{query}-{id}-',
+    autosave_restore_when_empty: false,
+    autosave_retention: '2m',
+    image_advtab: true,
+    importcss_append: true,
+    templates: [
+        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+        { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+        { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+    ],
+    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+    height: 600,
+    image_caption: true,
+    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+    noneditable_noneditable_class: 'mceNonEditable',
+    toolbar_mode: 'sliding',
+    spellchecker_ignore_list: ['Ephox', 'Moxiecode'],
+    tinycomments_mode: 'embedded',
+    content_style: '.mymention{ color: gray; }',
+    contextmenu: 'link image imagetools table configurepermanentpen',
+    a11y_advanced_options: true
 });
