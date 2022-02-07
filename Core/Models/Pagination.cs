e155cf5d52509f4fc7d518ed698cf49f60dd5570
Miguel Angel Castillo Cornejo <mikedosce1992@gmail.com>
2022-02-06 18:23:13 -0600

    Set deafult value for OrderBy and Direction properties in Pagination class

--- Core/Models/Pagination.cs
+++ Core/Models/Pagination.cs
@@ -2,13 +2,15 @@
 {
     public class Pagination
     {
-        public const int DefaultSize = 8;
-        
+        public const int DefaultSize = 12;
+        public const string DefaultOrdering = "title";
+        public const string DefaultDirection = "asc";
+
         public int Page { get; set; } = 1 ;
         public int Size { get; set; } = DefaultSize;
         public string Search { get; set; } = string.Empty;
-        public string OrderBy { get; set; } = string.Empty;
-        public string Direction { get; set; } = string.Empty;
+        public string OrderBy { get; set; } = DefaultOrdering;
+        public string Direction { get; set; } = DefaultDirection;
         
     }
 }
